d0cffca205848183a056a134cb2c176a7a040d68
Arthur <olehno.arturs@gmail.com>
2023-10-11 02:06:21 +0300

    - commit

---
+++ Netflix Clone/Controllers/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  Netflix Clone
+//
+//  Created by Артур Олехно on 09/10/2023.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
---
+++ Netflix Clone/Controllers/UpcomingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UpcomingViewController.swift
+//  Netflix Clone
+//
+//  Created by Артур Олехно on 09/10/2023.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
